ed63ff37f1cece7e76c0263ec0fe504a3977cc56
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-06-12 15:32:13 +0100

    feat(iam): user creation

--- init-iam/main.tf
+++ init-iam/main.tf
@@ -1,5 +1,26 @@
-module "devs_group" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-assumable-roles-policy"
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
   version = "5.1.0"
-  name    = "Developers"
+
+  name = "gabriel"
+
+  create_iam_user_login_profile = true
+  create_iam_access_key         = true
+}
+
+module "devs_group" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+  version = "5.1.0"
+  name    = "devs"
+
+  group_users = [
+    module.iam_user.iam_user_name,
+  ]
+
+  custom_group_policy_arns = [
+    "arn:aws:iam::aws:policy/PowerUserAccess",
+    "arn:aws:iam::aws:policy/IAMUserSSHKeys",
+    "arn:aws:iam::aws:policy/IAMUserChangePassword",
+    "arn:aws:iam::aws:policy/IAMSelfManageServiceSpecificCredentials",
+  ]
 }
