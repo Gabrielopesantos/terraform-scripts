5129810ceb3701a086a9fd0fa2c36bee24249f91
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-06-17 20:21:09 +0100

    Update to  grafana deployment

---
+++ kube-services/README.md
@@ -0,0 +1,32 @@
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_digitalocean"></a> [digitalocean](#requirement\_digitalocean) | ~> 2.0 |
+| <a name="requirement_helm"></a> [helm](#requirement\_helm) | ~> 2.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_digitalocean"></a> [digitalocean](#provider\_digitalocean) | 2.20.0 |
+| <a name="provider_helm"></a> [helm](#provider\_helm) | 2.5.1 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [helm_release.helm_grafana_release](https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release) | resource |
+| [digitalocean_kubernetes_cluster.cluster](https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/data-sources/kubernetes_cluster) | data source |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
--- kube-services/main.tf
+++ kube-services/main.tf
@@ -1,30 +1,37 @@
 data "digitalocean_kubernetes_cluster" "cluster" {
-  name = "main-cluster" # Hard coded
+  name = "main-cluster"
 }
 
+# resource "kubernetes_persistent_volume_claim_v1" "do_block_storage_pvc" {
+#   metadata {
+#     name = "block-storage-pvc"
+#     namespace = "grafana" // Won't exist
+#   }
+#   spec {
+#     access_modes = ["ReadWriteOnce"]
+#     resources {
+#       requests = {
+#         storage = "5Gi"
+#       }
+#     }
+#     volume_name = "cluster-do-block-storage"
+#     storage_class_name = "do-block-storage"
+#   }
+# }
+
 resource "helm_release" "helm_grafana_release" {
-  name       = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "grafana"
-  version    = "6.29.7"
+  name             = "grafana"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana"
+  version          = "6.30.1"
+  namespace        = "grafana"
+  create_namespace = true
 
-  #values = [
-  #"${file("values.yaml")}"
-  #]
-
-  #set {
-  #name  = "cluster.enabled"
-  #value = "true"
-  #}
-
-  #set {
-  #name  = "metrics.enabled"
-  #value = "true"
-  #}
-
-  #set {
-  #name  = "service.annotations.prometheus\\.io/port"
-  #value = "9127"
-  #type  = "string"
-  #}
+  values = [<<EOF
+persinstance:
+  enabled: true
+  existingClaim: "cluster-do-block-storage"
+  subPath: /var/lib/grafana/dashboards
+EOF
+  ]
 }
