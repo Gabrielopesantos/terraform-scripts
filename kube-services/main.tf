05cd93d5071c3e27e397cbf45afb05efabdbe8cf
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-06-22 00:14:50 +0100

    Fix persistant volume

--- kube-services/main.tf
+++ kube-services/main.tf
@@ -15,10 +15,11 @@ resource "kubernetes_persistent_volume_v1" "digital_ocean_main_volume" {
     persistent_volume_source {
       csi {
         driver        = "dobs.csi.digitalocean.com"
-        volume_handle = "611aadd3-f1af-11ec-b135-0a58ac14c292" // Is this sensitive?
+        volume_handle = "do:volume:613aadd3-f1af-11ec-b135-0a58ac14c292" // Is this sensitive?
         fs_type       = "ext4"
       }
     }
+    storage_class_name = "do-block-storage"
   }
 }
 
@@ -34,7 +35,8 @@ resource "kubernetes_persistent_volume_claim_v1" "digital_ocean_main_volume_pvc"
         storage = "5Gi"
       }
     }
-    volume_name = "${kubernetes_persistent_volume_v1.digital_ocean_main_volume.metadata.0.name}"
+    volume_name        = kubernetes_persistent_volume_v1.digital_ocean_main_volume.metadata.0.name
+    storage_class_name = "do-block-storage"
   }
 }
 
